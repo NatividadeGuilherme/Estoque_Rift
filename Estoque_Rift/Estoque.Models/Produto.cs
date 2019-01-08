da989abad04f687ad891e13dd4340eb65db59e0b
NatividadeGuilherme <guilhermenatividade450@hotmail.com>
2019-01-08 14:15:51 -0200

    dsasads

--- Estoque_Rift/Estoque.Models/Produto.cs
+++ Estoque_Rift/Estoque.Models/Produto.cs
@@ -8,7 +8,7 @@ namespace Estoque.Models
 {
     public class Produto
     {
-        public int Iddddd { get; set; }
+        public int Idd { get; set; }
         public string Nome { get; set; }
     }
 }
