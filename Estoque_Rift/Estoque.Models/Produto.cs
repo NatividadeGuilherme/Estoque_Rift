be9f4bf3760a27d64cb0d8bf15b8729b0f6bb2c4
Eduardo Fischernes Bastazzini <eduardo.bastazzini@rift.com.br>
2019-01-08 14:16:39 -0200

    bnbnnb

--- Estoque_Rift/Estoque.Models/Produto.cs
+++ Estoque_Rift/Estoque.Models/Produto.cs
@@ -8,7 +8,7 @@ namespace Estoque.Models
 {
     public class Produto
     {
-        public int Iddddd { get; set; }
+        public int Idddddkkkkkkk { get; set; }
         public string Nome { get; set; }
     }
 }
